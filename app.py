cc6a57cc00e0a17a31904c554589b4085cf8a0ad
Akashr-18 <akash.ravichandran18@gmail.com>
2024-01-23 18:14:38 +0530

    Requirements added

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+import pdf2image
+
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_gemini_response(input, pdf_content, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, pdf_content[0], prompt])
+    return response.text
